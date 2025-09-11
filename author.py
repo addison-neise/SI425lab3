508f3b553a3255c2d88c45528c9bb6886bfd9566
Neise <m264596@usna.edu>
2025-09-11 11:33:16 -0400

    Train Function

--- author.py
+++ author.py
@@ -10,13 +10,51 @@ import data
 # YOU MUST WRITE THESE TWO FUNCTIONS (train and test)
 #
 
+class Author_Classifier():
+
+    def __init__(self):
+        self.START = '<S>'
+        self.STOP = '</S>'
+        self.UNK = 'UNK'
+        self.wordcounts = dict()
+        self.wordcounts[self.STOP] = 0
+        self.bigramcounts = dict()
+        self.N = 0
+        self.V = 0
+
+    def train(self, passages):
+        '''
+        Given a list of passages and their known authors, train your learning model.
+        passages: a List of passage pairs (author,text) 
+        Returns: void
+        '''
+        # used gemini to find how to access the for author, passage in passages.
+
+        for author, passage in passages:
+            passage = passage.copy()
+            passage.append(self.STOP)
+            passage = ['<s>'] + passage
+
+            for w in passage:
+                if not w in self.wordcounts:
+                    self.wordcounts[w] = 1
+                else:
+                    self.wordcounts[w] += 1
+
+            for i in range(len(passage)):
+                w1 = passage[i - 1]
+                w2 = passage[i]
+                if not (w1, w2) in self.bigramcounts:
+                    self.bigramcounts[(w1, w2)] = 1
+                else:
+                    self.bigramcounts[(w1, w2)] += 1
+            self.N += len(passage)
+        pass
+
+#calling this function for the testing from Chambers
 def train(passages):
-    '''
-    Given a list of passages and their known authors, train your learning model.
-    passages: a List of passage pairs (author,text) 
-    Returns: void
-    '''
-    pass
+    x = Author_Classifier()
+    x.train(passages)
 
 
 def test(passages):
